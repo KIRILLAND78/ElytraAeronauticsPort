25d7f6f348ceb37b337d82ceaa179dbcc7ffac4b
KIRILLAND <55920722+KIRILLAND78@users.noreply.github.com>
2026-01-24 22:51:21 +0300

    1.21.6

--- src/main/java/com/github/Soulphur0/mixin/CloudRendererAccessors.java
+++ src/main/java/com/github/Soulphur0/mixin/CloudRendererAccessors.java
@@ -1,20 +1,14 @@
 package com.github.Soulphur0.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.CloudRenderMode;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.CloudRenderer;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.util.math.Vec3d;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(CloudRenderer.class)
 public interface CloudRendererAccessors {
-    @Invoker("renderClouds")
-    public void renderCloudsInvoker(int color, CloudRenderMode cloudRenderMode, float cloudHeight, Vec3d cameraPos, float ticks);
+    @Accessor("cells")
+    CloudRenderer.CloudCells getCells();
 
+    @Accessor("cells")
+    void setCells(CloudRenderer.CloudCells cells);
 }
